795eaf0609dcea77de31147fe11d2e41a4f7307d
Samuel Berthe <dev@samuel-berthe.fr>
2025-04-16 05:37:36 +0200

    chore(deps): upgrade rollup-plugin-terser

--- rollup.config.js
+++ rollup.config.js
@@ -2,7 +2,7 @@ import resolve from '@rollup/plugin-node-resolve';
 import typescript from 'rollup-plugin-typescript2';
 import commonjs from '@rollup/plugin-commonjs';
 import json from '@rollup/plugin-json';
-import { terser } from 'rollup-plugin-terser';
+import terser from '@rollup/plugin-terser';
 import pkg from './package.json' with {type: "json"};;
 
 const banner = `/*!
